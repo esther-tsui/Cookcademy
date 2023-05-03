18dea37d08c818da8177de535a3ab70ea4e73999
Esther Tsui <66723850+esther-tsui@users.noreply.github.com>
2023-05-03 21:37:11 +0100

    first commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Wing Hei Tsui on 03/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
